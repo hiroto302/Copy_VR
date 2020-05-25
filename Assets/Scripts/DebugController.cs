b256828ff16a658ede9d66d7f78430162cf8daaf
hiroto302 <i.am.a.hiroto@gmail.com>
2020-05-25 18:03:51 +0900

    FirstCommit

---
+++ Assets/Scripts/DebugController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour
+{
+    Rigidbody rb;
+    private float angleSpeed = 45.0f;
+    private float moveSpeed = 2.25f;
+    float x = 0;
+    float z = 0;
+    [SerializeField]
+    Transform moveTarget = null;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 1.0f, 0) * angleSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -1.0f, 0) * angleSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            x = -1.0f;
+        }
+        else
+        {
+            x = 0;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            z = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            z = -1.0f;
+        }
+        else
+        {
+            z = 0;
+        }
+
+        Vector3 move = (x * moveTarget.transform.right.normalized  + z * moveTarget.transform.forward.normalized  ) * moveSpeed * Time.deltaTime;
+        // Vector3 move = (x * moveTarget.transform.right.normalized  + z * moveTarget.transform.forward.normalized  );
+
+        Debug.Log(move.ToString("f5") + " : move");
+        transform.Translate(move, Space.World);
+    }
+}
---
+++ Assets/Scripts/PlayerControllerThird.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+// オキュラスクエスト用 transform.TranslateによるPlayerの移動方法
+// Translate の 移動法でカクツク原因は, time.deltaTimeを利用すると起こるのが一つの原因であった
+// 現在の解決方法
+// 1. FPSが約40であるため、その乗数をかける
+//    ＝＞ 対象機種がオキュラスクエストであってもデバイスによる性能差が出る場合が問題である
+
+// レンダリングの設定
+// カメラをキャラの動きに合わせる書き方をしてみる(Lateでカメラを後追いさせる)
+public class PlayerControllerThird : MonoBehaviour
+{
+    private float angleSpeed = 45.0f;
+    // private float moveSpeed = 2.0f;
+    private float moveSpeed = 0.01388f;
+    private float speedMultiplier = 0.02f;
+    [SerializeField]
+    Transform moveTarget = null;
+
+    // オキュラスクエストの入力値
+
+    float x, y;
+    Vector3 move = Vector3.zero;
+
+    Vector2 leftStick;
+    Vector2 rightStick;
+
+    void Reset()
+    {
+        if(!moveTarget)
+        {
+            moveTarget = GetComponentInChildren<OVRCameraRig>().transform.Find("TrackingSpace/CenterEyeAnchor");
+        }
+    }
+    void Update()
+    {
+        Debug.Log(Time.deltaTime.GetType());
+        // 左スティック入力 角度・旋回
+        // Vector2 leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        // transform.Rotate(new Vector3(0, leftStick.x, 0) * angleSpeed * speedMultiplier);
+        // transform.Rotate(new Vector3(0, leftStick.x, 0) * angleSpeed * Time.deltaTime);
+
+        // 右スティック入力 方向・移動
+        // Vector2 rightStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        rightStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        // x, y -1.0 ~ 1.0 の値
+        x = rightStick.x;
+        y = rightStick.y;
+        move = (x * moveTarget.right.normalized + y * moveTarget.forward.normalized ) * moveSpeed;
+        // move = (x * moveTarget.right.normalized + y * moveTarget.forward.normalized ) * moveSpeed * speedMultiplier;
+        // move = (x * moveTarget.right.normalized + y * moveTarget.forward.normalized ) * moveSpeed * Time.deltaTime;
+        // transform.Translate(move, Space.World);
+
+
+        OVRDebugConsole.Log(move.ToString("f5"));
+        // OVRDebugConsole.instance.AddMessage(move.ToString() + " : Time.deltaTime", Color.white);
+    }
+    
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, leftStick.x, 0) * angleSpeed);
+        transform.Translate(move, Space.World);
+    }
+}
